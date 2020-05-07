77e00fc179f2bcdf4ead33bfbd84a0ad9ccc9975
Victor <adevic4christ@gmail.com>
2020-05-07 23:29:27 +0100

    Created subjectController.js file

---
+++ controllers/subjectController.js
@@ -0,0 +1,18 @@
+const catchAsync = require("../utils/catchAsync");
+const AppError = require("../utils/AppError");
+
+exports.getAllSubjects = catchAsync(async (req, res, next) => {
+
+});
+exports.getSubject = catchAsync(async (req, res, next) => {
+
+});
+exports.createSubject = catchAsync(async (req, res, next) => {
+
+});
+exports.updateSubject = catchAsync(async (req, res, next) => {
+
+});
+exports.deleteSubject = catchAsync(async (req, res, next) => {
+
+});

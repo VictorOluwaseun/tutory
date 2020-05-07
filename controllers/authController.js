62999c17b03f1f91df9fa5c15c22c3981ac7cfa9
Victor <adevic4christ@gmail.com>
2020-05-07 22:12:48 +0100

    Added createSendToken funciton

--- controllers/authController.js
+++ controllers/authController.js
@@ -8,6 +8,25 @@ const signToken = id => jwt.sign({
   expiresIn: process.env.JWT_EXPIRES_IN
 });
 
+const createSendToken = (user, statusCode, res) => {
+  const token = signToken(user._id);
+  const cookieOptions = {
+    expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
+    httpOnly: true
+  };
+  if (process.env.NODE_ENV === "production") cookieOptions.secure = true;
+  res.cookie("jwt", token, cookieOptions);
+  //Remove password from the output
+  user.password = undefined;
+  res.status(statusCode).json({
+    status: "success",
+    token,
+    data: {
+      user
+    }
+  })
+}
+
 exports.login = catchAsync(async (req, res, next) => {
   const {
     email,
@@ -25,5 +44,5 @@ exports.login = catchAsync(async (req, res, next) => {
     return next(new AppError("Incorrect ID/email or password", 401))
   }
 
-
+  createSendToken(user, 200, res);
 });

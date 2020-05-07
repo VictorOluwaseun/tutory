d7b4f4d82fa1b2fa380d30c13ce518de0619a9bf
Victor <adevic4christ@gmail.com>
2020-05-08 00:00:42 +0100

    Updated subjectController by passing err

--- controllers/subjectController.js
+++ controllers/subjectController.js
@@ -8,6 +8,7 @@ exports.getAllSubjects = catchAsync(async (req, res, next) => {
     category: req.params.categoryId
   };
   const subjects = await Subject.find(filter);
+  if (!subjects.length) return next(new AppError("No subjects found", 404));
   res.status(200).json({
     status: "success",
     result: subjects.length,

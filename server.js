96e5f99abf1239cc45a00a48a61a9f9850d4e694
Victor <adevic4christ@gmail.com>
2020-05-07 22:42:45 +0100

    Created Database connection

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
+const mongoose = require("mongoose");
 const app = require("./app");
 const dotenv = require("dotenv");
 
 dotenv.config({
   path: "./config.env"
 });
+
+mongoose.connect(
+  process.env.DATABASE_LOCAL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }).then(() => console.log("DB connection successful"));
+
 
 const PORT = process.env.PORT;
 const HOSTNAME = process.env.LOCAL_SERVER

4bbd0e86c34c5afb7ff2ca0e32a612a944beb135
Victor <adevic4christ@gmail.com>
2020-05-07 22:20:43 +0100

    Added protect middleware

--- controllers/authController.js
+++ controllers/authController.js
@@ -59,3 +59,36 @@ exports.login = catchAsync(async (req, res, next) => {
 
   createSendToken(user, 200, res);
 });
+
+exports.protect = catchAsync(async (req, res, next) => {
+  //1. Getting token and check if it's there
+  let token;
+  if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
+    token = req.headers.authorization.split(" ")[1];
+  } else if (req.cookies.jwt) {
+    token = req.cookies.jwt;
+  }
+  // console.log(token);
+
+  if (!token) {
+    return next(new AppError("You are not logged in! Please log in to get access.", 401));
+  }
+  //2. Validate the token/ verification
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET); //async 
+
+  //3. Check if user still exists 
+  const currentUser = await User.findById(decoded.id); //To check if the user still exists
+
+  if (!currentUser) {
+    return next(new AppError("The user belonging to the token no longer exists.", 401));
+  }
+
+  //4. Check if user changed password after the token was issued
+  if (currentUser.changedPasswordAfter(decoded.iat)) {
+    return next(new AppError("User recently changed password! Please log in again.", 401));
+  }
+
+  //GRANT ACCESS TO PROTECTED ROUTE
+  res.locals.user = currentUser;
+  next();
+});

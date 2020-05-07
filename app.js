eebe54f28e478b8dcdaa29bdb3e0b7969f58bb2a
Victor <adevic4christ@gmail.com>
2020-05-07 22:38:18 +0100

    Imported userRoutes into app.js

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require("express");
+const userRouter = require("./routes/userRoutes");
 const app = express();
 
 //ROUTES
-// app.use("api/v1/users");
+app.use("api/v1/users", userRouter);
 // app.use("api/v1/subjects");
 // app.use("api/v1/categories");
 // app.use("api/v1/lessons");

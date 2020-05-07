0aa0cab1c1eb2e8acf4e599c1cf5818e1f2b3352
Victor <adevic4christ@gmail.com>
2020-05-07 23:37:11 +0100

    Updated getAllSubjects in userController

--- controllers/subjectController.js
+++ controllers/subjectController.js
@@ -1,9 +1,22 @@
+const Subject = require("../models/subjectModel");
 const catchAsync = require("../utils/catchAsync");
 const AppError = require("../utils/AppError");
 
 exports.getAllSubjects = catchAsync(async (req, res, next) => {
-
+  let filter = {};
+  if (req.params.categoryId) filter = {
+    category: req.params.categoryId
+  };
+  const subjects = await Subject.find(filter);
+  res.status(200).json({
+    status: "success",
+    result: subjects.length,
+    data: {
+      subjects
+    }
+  })
 });
+
 exports.getSubject = catchAsync(async (req, res, next) => {
 
 });

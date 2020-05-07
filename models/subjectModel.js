5f24628deecdc1e1ac63eb7f6c36920c7cbb2118
Victor <adevic4christ@gmail.com>
2020-05-07 22:56:58 +0100

    Updated subjectModel.js file

--- models/subjectModel.js
+++ models/subjectModel.js
@@ -1,7 +1,33 @@
 const mongoose = require("mongoose");
 
 const subjectSchema = new mongoose.Schema({
-
+  name: {
+    type: String,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    required: [true, "Provide the subject name"]
+  },
+  category: {
+    type: mongoose.Schema.ObjectId,
+    ref: "Category"
+  },
+  tutors: [{
+    type: mongoose.Schema.ObjectId,
+    ref: "User"
+  }],
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  },
+  slug: String,
+}, {
+  toJSON: {
+    virtuals: true
+  },
+  toObject: {
+    virtuals: true
+  }
 });
 
 const Subject = mongoose.model("Subject", subjectSchema);

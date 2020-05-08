c43d491a98091003ae1577074d2f6bf8e9c7eec7
Victor <adevic4christ@gmail.com>
2020-05-08 18:28:54 +0100

    Ready to implement register routes

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.use("api/v1/users", userRouter);
 app.use("api/v1/categories", categoryRouter);
 app.use("api/v1/subjects", subjectRouter);
 app.use("api/v1/lessons", lessonRouter);
+// app.use("api/v1/register")
 
 app.all("*", (req, res, next) => next(new AppError(`Can't find ${req.originalUrl} on this sever!`, 404)));
 

e008bc12537806a7c288dd59a874527c83c6c189
Victor <adevic4christ@gmail.com>
2020-05-08 19:48:52 +0100

    Added registerRouter to the Middleware stack

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const userRouter = require("./routes/userRoutes");
 const categoryRouter = require("./routes/categoryRoutes");
 const subjectRouter = require("./routes/subjectRoutes");
 const lessonRouter = require("./routes/lessonRoutes");
+const registerRouter = require("./routes/registerRoutes");
 const globalErrorHandler = require("./controllers/errorController");
 const AppError = require("./utils/AppError");
 const app = express();
@@ -16,7 +17,7 @@ app.use("api/v1/users", userRouter);
 app.use("api/v1/categories", categoryRouter);
 app.use("api/v1/subjects", subjectRouter);
 app.use("api/v1/lessons", lessonRouter);
-// app.use("api/v1/register")
+app.use("api/v1/register", registerRouter);
 
 app.all("*", (req, res, next) => next(new AppError(`Can't find ${req.originalUrl} on this sever!`, 404)));
 

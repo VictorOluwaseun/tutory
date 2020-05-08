f1cb339be2bab8283367271f0072733ee5d83e15
Victor <adevic4christ@gmail.com>
2020-05-08 21:49:17 +0100

    Added update password handler

--- controllers/authController.js
+++ controllers/authController.js
@@ -174,3 +174,22 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
   //4. Log the user in, send JWT
   createSendToken(user, 200, res);
 });
+
+exports.updatePassword = catchAsync(async (req, res, next) => {
+  //1. Get the user from collection
+  //since the user is already logged in, get id from JWT but user id on the request object
+  const user = await User.findById(req.user.id).select("+password");
+
+  //2. Check if the posted current password is correct
+  if (!(await user.correctPassword(req.body.passwordCurrent, user.password))) {
+    return next(new AppError("Your current password is wrong.", 401));
+  }
+
+  //3. If so, update password
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  await user.save();
+  //User.findByIdAndUpdate will NOT work as intended!
+  //4. Log User in, Send JWT
+  createSendToken(user, 200, res);
+});

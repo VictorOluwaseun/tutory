e9f98d0b6737069f719de0ab94a0f1294642573b
Victor <adevic4christ@gmail.com>
2020-05-08 20:35:11 +0100

    Modified createUser controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -89,48 +89,10 @@ exports.deleteMe = catchAsync(async (req, res, next) => {
 });
 
 exports.createUser = catchAsync(async (req, res, next) => {
-  if (req.user.role === "admin" || req.user.role === "tutor") {
-    if (req.body.role !== "tutor" && req.body.role !== "student") {
-      return (next(new AppError("No permission, Invalid input: " + req.body.role, 403)));
-    }
-
-    if (req.body.role === "tutor" && req.user.role === "student") {
-      next(new AppError("You do not have permission to perform this action", 403));
-    }
-
-    req.body.school = req.user.school;
-
-    const {
-      surname,
-      firstname,
-      uid,
-      photo,
-      role,
-      password,
-      passwordConfirm,
-      passwordChangedAt,
-      uClass,
-      subject,
-      school
-    } = req.body;
-
-    const newUser = await User.create({
-      surname,
-      firstname,
-      uid,
-      photo,
-      role,
-      password,
-      passwordConfirm,
-      passwordChangedAt,
-      uClass,
-      subject,
-      school
-    });
-
-    res.status(200).json({
-      status: "success",
-      data: newUser
-    })
-  }
+  //This is restricted to admin
+  const newUser = await User.create(req.body);
+  res.status(200).json({
+    status: "success",
+    data: newUser
+  })
 });

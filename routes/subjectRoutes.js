c23ebd05ed8f762e5985a8e776faec4db5cea11b
Victor <adevic4christ@gmail.com>
2020-05-08 08:15:37 +0100

    Updated subjectRoutes file

--- routes/subjectRoutes.js
+++ routes/subjectRoutes.js
@@ -1,9 +1,22 @@
 const express = require("express");
+const subjectController = require("../controllers/subjectController");
+const authController = require("../controllers/authController");
 
 const router = express.Router({
   mergeParams: true
 });
 
+router.use(authController.protect);
 
+router
+  .route("/")
+  .get(subjectController.getAllSubjects)
+  .post(authController.restrictTo("admin"), subjectController.setCategoryId, subjectController.createSubject);
+
+router
+  .route("/:id")
+  .get(subjectController.getSubject)
+  .patch(authController.restrictTo("admin"), subjectController.updateSubject)
+  .delete(authController.restrictTo("admin"), subjectController.deleteSubject);
 
 module.exports = router;

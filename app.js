820aa78b1082f613eeeddcbbf9c46f96f035d93e
Victor <adevic4christ@gmail.com>
2020-05-08 18:19:24 +0100

    Added route middlewares

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const userRouter = require("./routes/userRoutes");
 const categoryRouter = require("./routes/categoryRoutes");
+const subjectRouter = require("./routes/subjectRoutes");
+const lessonRouter = require("./routes/lessonRoutes");
 const globalErrorHandler = require("./controllers/errorController");
 const AppError = require("./utils/AppError");
 const app = express();
@@ -12,8 +14,8 @@ app.use(express.json());
 //ROUTES
 app.use("api/v1/users", userRouter);
 app.use("api/v1/categories", categoryRouter);
-// app.use("api/v1/subjects");
-// app.use("api/v1/lessons");
+app.use("api/v1/subjects", subjectRouter);
+app.use("api/v1/lessons", lessonRouter);
 
 app.all("*", (req, res, next) => next(new AppError(`Can't find ${req.originalUrl} on this sever!`, 404)));
 

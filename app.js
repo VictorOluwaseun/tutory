6a90d856f73e879a38543120ef53c2ea1477aa93
Victor <adevic4christ@gmail.com>
2020-05-08 14:39:18 +0100

    Added category routes

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const userRouter = require("./routes/userRoutes");
+const categoryRouter = require("./routes/categoryRoutes");
 const globalErrorHandler = require("./controllers/errorController");
 const AppError = require("./utils/AppError");
 const app = express();
 
 //ROUTES
 app.use("api/v1/users", userRouter);
+app.use("api/v1/categories", categoryRouter);
 // app.use("api/v1/subjects");
-// app.use("api/v1/categories");
 // app.use("api/v1/lessons");
 
 app.all("*", (req, res, next) => next(new AppError(`Can't find ${req.originalUrl} on this sever!`, 404)));
---
+++ routes/categoryRoutes.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const catergoryController = require("../controllers/categoryController");
+const authController = require("../controllers/authController");
+const subjectRouter = require("./subjectRoutes");
+
+const router = express.Router();
+
+router.use("/:categoryId/subjects", subjectRouter);
+
+router.use(authController.protect)
+
+router
+  .route("/")
+  .get(catergoryController.getAllCategories)
+  .post(authController.restrictTo("admin"), catergoryController.createCategory);
+
+router
+  .route("/:id")
+  .get(catergoryController.getCategory)
+  .patch(authController.restrictTo("admin"), catergoryController.updateCategory)
+  .delete(authController.restrictTo("admin"), catergoryController.deleteCategory);
+
+module.exports = router;

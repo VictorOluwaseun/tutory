d5f5147c88fcb8fc65616e0300272de33f28f84c
Victor <adevic4christ@gmail.com>
2020-05-08 14:44:32 +0100

    Created filter function

--- utils/apiFeatures.js
+++ utils/apiFeatures.js
@@ -2,7 +2,26 @@ class APIFeatures {
   constructor(query, queryString) {
     this.query = query;
     this.queryString = queryString;
+  }
 
+  filter() {
+    const queryObj = {
+      ...this.queryString
+    };
+    const excludedFields = ["page", "sort", "limit", "fields"];
+
+    excludedFields.forEach(el => delete queryObj[el]);
+    // console.log(queryObj);
+
+    //1b. Adevanced filetering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b{gte|gt|lte|lt}\b/g, match => `$${match}`);
+
+    // console.log(JSON.parse(queryStr));
+
+    this.query = this.query.find(JSON.parse(queryStr));
+    // let query = Tour.find(JSON.parse(queryStr));
+    return this;
   }
 }
 

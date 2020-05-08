fd22eeab19b28bc94044786f8433f307d39a8f85
Victor <adevic4christ@gmail.com>
2020-05-08 21:51:40 +0100

    Added auth routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,6 +4,13 @@ const authController = require("../controllers/authController");
 
 const router = express.Router();
 
+router.post("/signup", authController.signup);
+router.post("/login", authController.login);
+router.get("/logout", authController.logout);
+router.post("/forgotPassword", authController.forgotPassword);
+router.patch("/resetPassword/:token", authController.resetPassword);
+
+
 router.use(authController.protect);
 
 router.patch("/updateMyPassword", authController.updatePassword);

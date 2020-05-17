3544aac0e542e40e7e71fc815f82c4443f7e451d
Victor <adevic4christ@gmail.com>
2020-05-17 12:51:39 +0100

    Updated user routes and user controller

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -14,28 +14,32 @@ router.get("/logout", authController.logout);
 router.post("/forgotPassword", authController.forgotPassword);
 router.patch("/resetPassword/:token", authController.resetPassword);
 
-
+//Protect all routes after this middleware
 router.use(authController.protect);
 
 router.patch("/updateMyPassword", authController.updatePassword);
-// router.get("/me", userController.getMe, userController.getUser);
+router.get("/me", userController.getMe, userController.getUser);
 router.patch("/updateMe", userController.uploadUserPhoto, userController.resizeUserPhoto, userController.updateMe);
 router.delete("/deleteMe", userController.deleteMe);
+
+router.get("/search/tutors", authController.restrictTo("admin", "student"), userController.getAllTutors, userController.getAllUsers);
+
+//Admin routes
+router.use(authController.restrictTo("admin"));
+
+router.get("/tutors", userController.getAllTutors, userController.getAllUsers);
 
 router
   .route("/")
   .get(authController.restrictTo("admin"), userController.getAllUsers)
   .post(authController.restrictTo("admin"), userController.createUser);
 
-router.get("/tutors", authController.restrictTo("admin"), userController.getAllTutors, userController.getAllUsers);
 
-router.get("/search/tutors", authController.restrictTo("admin", "student"), userController.getAllTutors, userController.getAllUsers);
 
 router
   .route("/tutors/:id")
   .get(authController.restrictTo("admin"), userController.getAllTutors, userController.getUser)
   .post(authController.restrictTo("admin"), userController.updateUser);
-
 
 router
   .route("/:id")
